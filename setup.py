088cfe843dacf2c9cff56c58e1f235cc1ab243d0
dani <29378233+DanielleMiu@users.noreply.github.com>
2024-07-13 18:08:28 -0400

    fix requires-python

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	url='https://github.com/kheina-com/avrofastapi',
 	packages=find_packages(exclude=['tests']),
 	install_requires=list(filter(None, map(str.strip, open('requirements.txt').read().split()))),
-	python_requires='>=3.7.*',
+	python_requires='>=3.12',
 	license='Mozilla Public License 2.0',
 	extras_require=dict(map(lambda x : (x[1], open(x[0]).read().split()), filter(None, map(req_regex.match, listdir())))),
 )
